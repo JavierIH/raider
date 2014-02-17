e56b46a311c39a34f99638dd2ea94981736d2074
Demo User <debian@arm.(none)>
2014-02-17 05:19:07 +0000

    Lecturas analogicas

---
+++ programming/sandbox/analog/analog.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+	cout<<"Hola Raider"<<endl;
+	ifstream AIN0 ("/sys/bus/platform/drivers/bone-iio-helper/helper.15/AIN0");
+	char a1 [10];
+	AIN0.getline(a1,6);
+	cout<<a1<<endl;
+}
