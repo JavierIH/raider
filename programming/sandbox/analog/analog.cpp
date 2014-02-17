cb6a16725c5cca31532ded7fd9798a53d34333d0
JavierIH <javier.i.h@hotmail.com>
2014-02-17 05:37:09 +0000

    Prueba de idnentidad 2

--- programming/sandbox/analog/analog.cpp
+++ programming/sandbox/analog/analog.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-	cout<<"Hola Raider"<<endl;
+	cout<<"Hola, Raider"<<endl;
 	ifstream AIN0 ("/sys/bus/platform/drivers/bone-iio-helper/helper.15/AIN0");
 	char a1 [10];
 	AIN0.getline(a1,6);
