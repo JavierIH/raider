8bdfe53dd170b016a9ce852e5cd8d0a62f486784
JavierIH <javier.i.h@hotmail.com>
2014-02-17 05:22:12 +0000

    BBB commit

--- programming/src/test/test.cpp
+++ programming/src/test/test.cpp
@@ -10,10 +10,10 @@ int main(){
     cout<<"\nGO!";
     raider Raider;
     Raider.getIR(1);
-    //while(1){
-    int error=Raider.sendCommand('w');
-cout<<"\n\nERROR"<<error;
-        //sleep(1);
-    //}
+    while(1){
+    int error=Raider.sendCommand(9);
+	cout<<"\n\n\nEnviado un 9 ("<<error<<")"<<endl;
+        sleep(1);
+    }
     return 0;
 }
