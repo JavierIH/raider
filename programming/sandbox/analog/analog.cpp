369543689b7d140f8b1f7ba73449cb26a6616532
JavierIH <javier.i.h@hotmail.com>
2014-02-17 06:29:36 +0000

    Analog funcionando

--- programming/sandbox/analog/analog.cpp
+++ programming/sandbox/analog/analog.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(){
 
-	cout<<"Hola, Raider"<<endl;
-	ifstream AIN0 ("/sys/bus/platform/drivers/bone-iio-helper/helper.15/AIN0");
-	char a1 [10];
-	AIN0.getline(a1,6);
-	cout<<a1<<endl;
+	int value;
+	char a1 [5];
+	for(int i=0; i<20; i++){
+		ifstream AIN0 ("/sys/bus/platform/drivers/bone-iio-helper/helper.15/AIN0");
+		AIN0.read(a1,5);
+		value = atoi(a1);
+		cout<<value<<endl;
+		AIN0.close();
+		sleep(1);
+	}
 }
