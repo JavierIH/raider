dba3ef35b94a3fc2e5d489ef523b9ef620a35bbe
JavierIH <javier.i.h@hotmail.com>
2014-02-17 05:40:38 +0000

    (From BBB) Test_imu FUNCIONAAAA

--- programming/src/test/test_imu.cpp
+++ programming/src/test/test_imu.cpp
@@ -21,19 +21,24 @@ int main() {
 
         if(65535==imu.getMagnetometerX()) report(WARNING, "Getting accelerometer may be not working on X axis");
         else{
-            cout<<"Magnetometer X: "<<imu.getMagnetometerX()<<endl;
-            cout<<"Magnetometer Y: "<<imu.getMagnetometerY()<<endl;
-            cout<<"Magnetometer Z: "<<imu.getMagnetometerZ()<<endl<<endl;
+            //cout<<"Magnetometer X: "<<imu.getMagnetometerX()<<endl;
+            //cout<<"Magnetometer Y: "<<imu.getMagnetometerY()<<endl;
+            //cout<<"Magnetometer Z: "<<imu.getMagnetometerZ()<<endl<<endl;
 
-            cout<<"Acceleration X: "<<imu.getAccelerometerX()<<endl;
-            cout<<"Acceleration Y: "<<imu.getAccelerometerY()<<endl;
-            cout<<"Acceleration Z: "<<imu. getAccelerometerZ()<<endl<<endl;
+	    int ax=imu.getAccelerometerX()*360/65355;
 
-            cout<<"Gyroscope X: "<<imu.getGyroscopeX()<<endl;
-            cout<<"Gyroscope Y: "<<imu.getGyroscopeY()<<endl;
-            cout<<"Gyroscope Z: "<<imu.getGyroscopeZ()<<endl<<endl;
+            cout<<"Acceleration X: "<<ax<<endl;
+            cout<<"Acceleration Y: "<<imu.getAccelerometerY()*360/65355<<endl;
+            cout<<"Acceleration Z: "<<imu. getAccelerometerZ()*360/65335<<endl<<endl;
 
-            cout<<"Temperature: "<<imu.getTemp()<<endl<<endl;
+	    if(ax>300||ax<60) report(OK,"En pie");
+	    else report(ERROR,"Caida");
+
+            //cout<<"Gyroscope X: "<<imu.getGyroscopeX()<<endl;
+            //cout<<"Gyroscope Y: "<<imu.getGyroscopeY()<<endl;
+            //cout<<"Gyroscope Z: "<<imu.getGyroscopeZ()<<endl<<endl;
+
+            //cout<<"Temperature: "<<imu.getTemp()<<endl<<endl;
 
             usleep(100000);
         }
