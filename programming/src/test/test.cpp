6091e06c8f10705ecb4c3e66fcea1edcc816a2db
JavierIH <javier.i.h@hotmail.com>
2014-04-15 16:29:49 +0200

    mod

--- programming/src/test/test.cpp
+++ programming/src/test/test.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 
+#include "../libraries/raider/raider.h"
+
 using namespace std;
+
 
 int main(){
 
-    cout<<"Todo es correcto";
+    cout<<"\nGO!";
+    raider Raider;
+    Raider.getIR(1);
+    //while(1){
+        Raider.sendCommand('w');
+        //sleep(1);
+    //}
     return 0;
 }
