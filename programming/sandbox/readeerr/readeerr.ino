aa449ee2a849497f8285a11c9eb345fa9bb23f2b
Javier Isabel Hernandez <javier.i.h@hotmail.com>
2014-04-17 00:20:31 +0200

    cosas varias y me cago en la puta fecha otra vez

--- programming/sandbox/readeerr/readeerr.ino
+++ programming/sandbox/readeerr/readeerr.ino
@@ -2,13 +2,13 @@
 
 void setup() {
   SerialUSB.begin();
-  Serial3.begin(115200);
+  Serial3.begin(9600);
+      pinMode(BOARD_LED_PIN, OUTPUT);
+
 }
 void loop() {
   if(Serial3.available()){
     SerialUSB.println(Serial3.read());
   }
-  SerialUSB.println("ok");
-  delay(3000);
-}
+ }
 
