d1cb60606cdeb23d2ab1a197367ba34d6357fad6
Javier Isabel Hernandez <javier.i.h@hotmail.com>
2014-07-08 18:14:06 +0200

    Motion

--- programming/opencm/motion/motion.ino
+++ programming/opencm/motion/motion.ino
@@ -14,25 +14,24 @@ void loop() {
 //    SerialUSB.println("ok");
 
 //while(1);
-int amp=60;
+int amp=60; 
 float tiempo=0.2;
 int altura_paso=80;
 int balanceo=35;
-                          //   P  T  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18 
+int m=30; 
+
      raider.setTargetPosition(512,0,512,512,212,812,662,362,512,512,512,512,492,532,512,512,512,512,512,512);
      //raider.movLateral(-15,15);
           raider.move(tiempo); 
+          delay(50);
 
-     
 
 
-  
+//PASO INICIAL IZQUIERDO
    raider.movVertical(-40,-40);
    raider.move(tiempo); 
-
    raider.movLateral(balanceo,balanceo);
    raider.move(tiempo); 
-
    raider.movVertical(-altura_paso,40); //Subir pie
    raider.movFrontal(10,-10);
    raider.move(tiempo/2); 
@@ -42,14 +41,12 @@ int balanceo=35;
    raider.move(tiempo*2); 
    delay(50);
    
+   for (int i=0;i<12;i++){
+//PASO DERECHO 
    raider.movVertical(-40,-40);
    raider.move(tiempo); 
-
    raider.movLateral(-balanceo,-balanceo);
    raider.move(tiempo*2); 
-
-
-      
    raider.movVertical(40,-altura_paso); //subir pie
    raider.movFrontal(-20,20);
    raider.move(tiempo/2); 
@@ -57,7 +54,36 @@ int balanceo=35;
    raider.move(tiempo*2);
    raider.movLateral(balanceo,balanceo);
    raider.move(tiempo*2);
-     delay(50);
+   delay(50);
+
+//PASO IZQUIERDO 
+   raider.movVertical(-40,-40);
+   raider.move(tiempo); 
+   raider.movLateral(balanceo,balanceo);
+   raider.move(tiempo*2); 
+   raider.movVertical(-altura_paso,40); //subir pie
+   raider.movFrontal(20,-20);
+   raider.move(tiempo/2); 
+   raider.movVertical(40+altura_paso,0);
+   raider.move(tiempo*2);
+   raider.movLateral(-balanceo,-balanceo);
+   raider.move(tiempo*2);
+   delay(50);
+   }
+   
+//PASO INICIAL IZQUIERDO
+   raider.movVertical(-40,-40);
+   raider.move(tiempo); 
+   raider.movLateral(-balanceo,-balanceo);
+   raider.move(tiempo); 
+   raider.movVertical(40,-altura_paso); //Subir pie
+   raider.movFrontal(-10,10);
+   raider.move(tiempo/2); 
+   raider.movVertical(0,40+altura_paso);//Bajar pie
+   raider.move(tiempo*2); 
+   raider.movLateral(balanceo,balanceo);
+   raider.move(tiempo*2); 
+   delay(5000);
      
 
 
