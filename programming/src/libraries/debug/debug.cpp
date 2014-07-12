be20f08357e8613c90f0d31b2d6009f9279b6449
JavierIH <javier.i.h@hotmail.com>
2014-07-12 16:11:57 +0200

    Detalles minimos

--- programming/src/libraries/debug/debug.cpp
+++ programming/src/libraries/debug/debug.cpp
@@ -4,16 +4,16 @@ void report(int type, std::string msg){
 
     switch ( type ) {
     case ERROR:
-        std::cout<<"\033[1;31m[ERROR] "+msg+"\033[0m"<<endl;
+        std::cout<<"\033[1;31m[ERROR]   "+msg+"\033[0m"<<endl;
         break;
     case WARNING:
-        std::cout<<"\033[1;30m[WARNING] "+msg+"\033[0m"<<endl;
+        std::cout<<"\033[1;33m[WARNING]\033[0m \033[0;33m"+msg+"\033[0m"<<endl;
         break;
     case OK:
-        std::cout<<"\033[1;32m[OK] "+msg+"\033[0m"<<endl;
+        std::cout<<"\033[1;32m[OK]     "+msg+"\033[0m"<<endl;
         break;
     case INFO:
-        std::cout<<"\033[1;34m[INFO] "+msg+"\033[0m"<<endl;
+        std::cout<<"\033[1;34m[INFO]   \033[0m \033[0;34m"+msg+"\033[0m"<<endl;
         break;
     default:
         std::cout<<msg<<endl;
