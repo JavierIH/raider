de7f0dc72476d9d1d328f07f6d260af1c903c106
Javier Isabel Hernandez <javier.i.h@hotmail.com>
2014-07-16 17:12:56 +0200

    readerr

--- programming/sandbox/readeerr/readeerr.ino
+++ programming/sandbox/readeerr/readeerr.ino
@@ -3,12 +3,21 @@
 void setup() {
   SerialUSB.begin();
   Serial3.begin(9600);
-      pinMode(BOARD_LED_PIN, OUTPUT);
+  pinMode(BOARD_LED_PIN, OUTPUT);
 
 }
 void loop() {
+       digitalWrite(BOARD_LED_PIN, HIGH);
+
   if(Serial3.available()){
-    SerialUSB.println(Serial3.read());
+    SerialUSB.println((char)Serial3.read());
+   digitalWrite(BOARD_LED_PIN, LOW);
+   delay(1000);
+   digitalWrite(BOARD_LED_PIN, HIGH);
+   delay(1000);
+   Serial3.flush();
   }
+
+
  }
 
