bc1e3cc67ecce68f0c24c1f82da732b5ac38f08f
Javier Isabel Hernandez <javier.i.h@hotmail.com>
2014-08-07 17:12:22 +0200

    Pasos laterales pulidos

--- programming/opencm/motion/motion.ino
+++ programming/opencm/motion/motion.ino
@@ -28,33 +28,9 @@ int altura_paso=10;
 int avance=40;
 int amplitud=30;
 
+     raider.stepL();
 
-     raider.setTargetPosition(512,0,462,562,212,812,992,32,512,512,512,512,462,562,512,512,512+5,512-5,512,512);
-     raider.movVertical(-120,-120);
-     raider.setTargetOffset(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-     raider.movLateral(20,-20);
-     raider.move(0.1);
-     
-  
-  
-     raider.movLateral(avance,-avance);
-     raider.setTargetOffset(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,40);
-     raider.movVertical(0,-30);
-     raider.move(0.1);
-     
-     raider.setTargetOffset(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-20,0);
-     raider.move(0.1);
-     
-     raider.movLateral(-avance,avance);
-     raider.setTargetOffset(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-40);
-     raider.movVertical(-30,0);
-     raider.move(0.1);
-
-
-     
-
-
-  delay(3000);
+  delay(300);
 
    
 
