f67999954347ccaa80e1ed9409ab11ff5628455b
JavierIH <javier.i.h@hotmail.com>
2014-08-30 20:13:20 +0200

    brujular

--- programming/src/libraries/compass/compass.cpp
+++ programming/src/libraries/compass/compass.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 Compass::Compass(I2C *connection) {
     i2c = connection;
-
-    //Modo de medida continua (despertar)
-    if(i2c->write8(COMPASS_I2C_ADDRESS,0x02,0x00)<0) report(ERROR, "Compass connection failed (not waking up)");
 }
 
 Compass::~Compass() {
